782c5b3c406e1462561e72388f75e2bc087be41e
gapela <gabrielpelaicontato@gmail.com>
2025-04-22 13:25:54 -0300

    add: Att requirements, add functions and main script

---
+++ functions.py
@@ -0,0 +1,15 @@
+# LIBS
+import pandas as pd
+import sklearn as sk
+
+def data_import():
+    pass
+
+def data_normalize():
+    pass
+
+def df_maker():
+    pass
+
+def git_backup_control():
+    pass
